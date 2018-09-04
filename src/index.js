7f5214dc083c8cabb30ef4b529c980da028555fa
Tim <tim.f.wilkins@gmail.com>
2018-09-04 11:13:39 -0500

    09/04
    09/04

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search'
-import SearchBar from './components/search_bar'
-import VideoList from './components/video_list'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import Lol from "./components/lol";
 
-const API_KEY = 'AIzaSyAIHYY5zNFvwi7tr2mO1rAxC1q6v3Ole5w'
+const API_KEY = "AIzaSyAIHYY5zNFvwi7tr2mO1rAxC1q6v3Ole5w";
 
-class App extends Component  {
-    constructor(props) {
-        super(props);
-        this.state = { videos: [] };
-        YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
-            this.setState({videos})
-            //this.setState({videos:videos}), key and property same variable
-        });
-    }
-    render() {
-        return (
-    <div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: "skateboards" }, videos => {
+      this.setState({ videos });
+      //this.setState({videos:videos}), key and property same variable
+    });
+  }
+  render() {
+    return (
+      <div>
         <SearchBar />
         <VideoList videos={this.state.videos} />
-    </div>
-    )
-}
+        <Lol />
+      </div>
+    );
+  }
 }
 
-
-ReactDOM.render(<App />,document.querySelector('.container')); 
+ReactDOM.render(<App />, document.querySelector(".container"));
